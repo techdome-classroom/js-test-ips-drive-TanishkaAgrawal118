f939c2a1e6e266136420ed93c866175a5e2e438b
Tanishka Agrawal <tanishkaagrawal118@gmail.com>
2024-04-26 13:25:07 +0530

    a

--- program1.js
+++ program1.js
@@ -1,7 +1,13 @@
 function smallestMissingPositiveInteger(nums) {
-
+const numSet = new Set(nums);
+let smallestMissing =1;
+while(numSet.has(smallestMissing)){
+  smallestMissing++;
+}
+return smallestMissing;
 
     // Implement the function smallest_missing_positive_integer
+
   
   }
   
